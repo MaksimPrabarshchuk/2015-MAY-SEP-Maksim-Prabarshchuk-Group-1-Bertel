a38bd225cf620f5358f41d3dae538a2823f9de69
Antoni Bertel <Antoni_Bertel@epam.com>
2015-05-18 18:40:22 +0300

    Create structure of gradle multimodule project, added empty classes.

---
+++ src/main/java/by/epam/mentoring/classloader/CustomClassLoader.java
@@ -0,0 +1,11 @@
+package by.epam.mentoring.classloader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by Antoni Bertel on 17.05.2015.
+ */
+public class CustomClassLoader extends ClassLoader {
+    static final Logger LOGGER = LogManager.getLogger(CustomClassLoader.class.getName());
+}
---
+++ src/main/java/by/epam/mentoring/classloader/Starter.java
@@ -0,0 +1,15 @@
+package by.epam.mentoring.classloader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by Antoni Bertel on 17.05.2015.
+ */
+public class Starter {
+    static final Logger LOGGER = LogManager.getLogger(Starter.class.getName());
+
+    public static void main(String args[]) {
+
+    }
+}
