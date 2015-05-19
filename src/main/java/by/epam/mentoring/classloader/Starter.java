d506196bdeda4dc9fbe1c1b79236b0e683e4db3e
Antoni <antbert@mail.ru>
2015-05-19 10:20:19 +0300

    Changes in log4j console writer pattern, system.out replaced by logger

--- src/main/java/by/epam/mentoring/classloader/Starter.java
+++ src/main/java/by/epam/mentoring/classloader/Starter.java
@@ -12,23 +12,21 @@ import java.util.jar.JarFile;
 public class Starter {
     private static final Logger LOGGER = LogManager.getLogger(Starter.class.getName());
     public static final String USER_DIR = "user.dir";
+    public static final int MINIMAL_ARGS_SIZE = 1;
 
     public static void main(String args[]) {
-        if (args.length < 1) {
+        if (args.length < MINIMAL_ARGS_SIZE) {
             LOGGER.info("Expect a relative path to jar");
             return;
         }
         final String jarName = args[0];
         final String workingDir = System.getProperty(USER_DIR);
-        LOGGER.info("Current working dir = " + workingDir);
+        LOGGER.info("Current working dir = {} ", workingDir);
         CustomClassLoader myClassLoader = new CustomClassLoader();
         try {
             myClassLoader.loadJar(new JarFile(workingDir + "/" + jarName));
         } catch (IOException e) {
             LOGGER.error(e);
-            System.out.println("Please provide correct path to jar");
         }
-
-
     }
 }
