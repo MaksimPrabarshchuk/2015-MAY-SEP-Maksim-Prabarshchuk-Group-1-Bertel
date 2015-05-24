f77649b20011c060cc9d0721c186dacdb0271d83
Antoni <antbert@mail.ru>
2015-05-25 00:09:06 +0300

    Added changes in classloader module, created gc module, added gc parameters to resource file

--- jmp-01-classloader/src/main/java/by/epam/mentoring/classloader/CustomClassLoader.java
+++ jmp-01-classloader/src/main/java/by/epam/mentoring/classloader/CustomClassLoader.java
@@ -6,8 +6,6 @@ import org.apache.logging.log4j.Logger;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 /**
@@ -27,32 +25,21 @@ public class CustomClassLoader extends ClassLoader {
         this.jarFile = jarfile;
     }
 
-
     @Override
-    public Class<?> findClass(String className) {
-        Enumeration<JarEntry> entries = this.jarFile.entries();
-        while (entries.hasMoreElements()) {
-            JarEntry jarEntry = entries.nextElement();
-            if (jarEntry == null || jarEntry.isDirectory() || !jarEntry.getName().endsWith(".class")) {
-                continue;
+    protected Class<?> findClass(String className) {
+        try {
+            InputStream in = this.jarFile.getInputStream(this.jarFile.getEntry(className.replace(".", "/").replace("/class", ".class")));
+            byte[] array = new byte[in.available()];
+            ByteArrayOutputStream out = new ByteArrayOutputStream(array.length);
+            int length = in.read(array);
+            while (length > 0) {
+                out.write(array, 0, length);
+                length = in.read(array);
             }
-            try {
-                String currentClassName = jarEntry.getName().replaceAll("/", "\\.");
-                if (className.equalsIgnoreCase(currentClassName)) {
-                    InputStream in = this.jarFile.getInputStream(jarEntry);
-                    byte[] array = new byte[in.available()];
-                    ByteArrayOutputStream out = new ByteArrayOutputStream(array.length);
-                    int length = in.read(array);
-                    while (length > 0) {
-                        out.write(array, 0, length);
-                        length = in.read(array);
-                    }
-                    LOGGER.info("Found class {}", className);
-                    return defineClass(className.replace(".class", ""), out.toByteArray(), 0, out.size());
-                }
-            } catch (Exception e) {
-                LOGGER.error(e);
-            }
+            LOGGER.info("Found class {}", className);
+            return defineClass(className.replace(".class", ""), out.toByteArray(), 0, out.size());
+        } catch (Exception e) {
+            LOGGER.error(e);
         }
         LOGGER.error("Can't find class {}", className);
         return null;
